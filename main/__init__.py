cad830fe2b439f61162d663afec04e1b04d9e08e
Xhesas <namnak.xhesas@gmail.com>
2026-02-23 03:30:17 +0100

    misspelling

--- main/__init__.py
+++ main/__init__.py
@@ -283,4 +283,4 @@ if __name__ == '__main__':
         app.wsgi_app = ProxyFix(
             app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
         )
-    app.run(port=5001, debut=args.Debug)
+    app.run(port=5001, debug=args.Debug)
